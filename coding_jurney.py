02f4b83a76dfafbfb8dc7b760ec14666aa6e116b
M. ROBBI AL HAKIM <143859725+Alhabi19@users.noreply.github.com>
2023-09-02 10:58:15 +0700

    Add files via upload

---
+++ coding_jurney.py
@@ -0,0 +1,25 @@
+nama_saya = str(input("masukan nama kamu ="))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = int(input("masukan umur kamu ="))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("masukan IPK kamu ="))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input("menikah? ="))
+print("menikah?", "menikah" "bertipe", type(menikah))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
